420db9c6c21a53e86529377bddfb36962f8a965c
Ganes556 <gustiganes@gmail.com>
2024-05-26 22:25:06 +0800

    fix: remove record content image

--- internal/entity/news.go
+++ internal/entity/news.go
@@ -4,13 +4,12 @@ type UrlImages []string
 
 type News struct {
 	Base
-	UsersID       uint        `gorm:"not null"`
-	CategoriesID  uint        `gorm:"not null"`
-	Categories    *Categories `json:"-"`
-	Users         *Users
-	Title         string    `gorm:"type:varchar(150);uniqueIndex;not null"`
-	Cover         string    `gorm:"type:varchar(255);not null"`
-	ContentImages UrlImages `gorm:"serializer:json"`
-	Content       string    `gorm:"not null"`
-	CountView     uint
+	UsersID      uint        `gorm:"not null"`
+	CategoriesID uint        `gorm:"not null"`
+	Categories   *Categories `json:"-"`
+	Users        *Users
+	Title        string `gorm:"type:varchar(150);uniqueIndex;not null"`
+	Cover        string `gorm:"type:varchar(255);not null"`
+	Content      string `gorm:"not null"`
+	CountView    uint
 }
